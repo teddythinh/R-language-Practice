248ce72dfadd00afad964bbf5dd49b5f18d8a67f
Phạm Huy Cường Thịnh <85171754+teddythinh@users.noreply.github.com>
2022-01-15 17:27:54 +0700

    Create BooleanAndIfElse.r

---
+++ BooleanAndIfElse.r
@@ -0,0 +1,21 @@
+a <- 33
+b <- 33
+
+if (b > a) {
+    print("b is greater than a")
+} else if (a == b) {
+    print ("a and b are equal")
+}
+
+x <- 41
+
+if (x > 10) {
+    print("Above ten")
+    if (x > 20) {
+        print("and also above 20!")
+    } else {
+        print("but not above 20.")
+    }
+} else {
+    print("below 10.")
+}
